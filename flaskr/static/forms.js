4d3bc8b3f50ca6c862df283fe78e59a0935c31e8
Katherine456719 <alexandra.lung@s.unibuc.ro>
2023-03-12 22:34:52 +0200

    Added frotend for error message in forms

--- flaskr/static/forms.js
+++ flaskr/static/forms.js
@@ -1,7 +1,28 @@
 function checkFunction() {
-    if ($("#pass").attr('type') === "password") {
-        $("#pass").get(0).type = 'text';
-    } else {
-        $("#pass").get(0).type = "password";
+    let pass_field = $("#passo")
+    let conf_pass_field = $("#confpass")
+    if (pass_field.length) {// element exists
+        if (pass_field.attr('type') === "password") {
+            pass_field.get(0).type = 'text';
+        } else {
+            pass_field.get(0).type = "password";
+        }
     }
+    if (conf_pass_field.length) {            // element exists
+        if (conf_pass_field.attr('type') === "password") {
+            conf_pass_field.get(0).type = 'text';
+        } else {
+            conf_pass_field.get(0).type = "password";
+        }
+    }
+
 }
+
+let err_msg_box = $(".err_msg")
+if (err_msg_box.length) {
+    err_msg_box.slideDown("slow");
+    err_msg_box.delay(1500);
+    err_msg_box.slideUp("slow");
+}
+
+
