2b3172927064dab7c93dc1a290ed664ab80a987b
visan <alexandruvisan44@yahoo.com>
2023-03-13 21:55:15 +0200

    Added socket.io support.

---
+++ app.py
@@ -0,0 +1,11 @@
+import flaskr
+from sockets.board import BoardNamespace
+from flask_socketio import SocketIO
+
+app = flaskr.create_app()
+socketio = SocketIO(app)
+
+socketio.on_namespace(BoardNamespace("/board"))
+
+if __name__ == "__main__":
+    socketio.run(app, allow_unsafe_werkzeug=True, debug=True)
---
+++ sockets/board.py
@@ -0,0 +1,12 @@
+from flask_socketio import Namespace, emit
+
+
+class BoardNamespace(Namespace):
+    def on_connect(self):
+        pass
+
+    def on_disconnect(self):
+        pass
+
+    def on_my_event(self, data):
+        emit('my_response', data)
