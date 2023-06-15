a47e888029060efa9cb930eeab70bd25dda06f03
visan <alexandruvisan44@yahoo.com>
2023-06-15 14:06:50 +0300

    Bind to 0.0.0.0 instead of localhost to fix heroku port binding problem.

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ socketio = SocketIO(app)
 socketio.on_namespace(BoardNamespace("/board"))
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, log_output=True)
+    socketio.run(app, host="0.0.0.0", log_output=True)
