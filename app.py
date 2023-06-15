4ff85fb15daa3d3e4198663ab6b76c1e7ddf0577
visan <alexandruvisan44@yahoo.com>
2023-06-15 13:45:30 +0300

    Preparing for deployment.

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ socketio = SocketIO(app)
 socketio.on_namespace(BoardNamespace("/board"))
 
 if __name__ == "__main__":
-    socketio.run(app, allow_unsafe_werkzeug=True, debug=True, log_output=True)
+    socketio.run(app, debug=True, log_output=True)
